9b5fbe129319ff12da7c18816b63be5c460b8365
thecontinium <thecontinium@outlook.com>
2016-11-23 15:13:57 +0800

    New

---
+++ Formula/upgradenvim.rb
@@ -0,0 +1,12 @@
+class Upgradenvim < Formula
+  homepage 'https://github.com/thecontinium/homebrew-upgradenvim/'
+  url 'https://github.com/thecontinium/homebrew-upgradenvim.git'
+  version '1.0.0'
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'upgrade_nvim'
+  end
+
+end
