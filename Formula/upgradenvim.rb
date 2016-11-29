3acf817028f0a79d0750ca3330c9b998cdeae67d
thecontinium <thecontinium@outlook.com>
2016-11-29 15:04:03 +0800

    force brew to link

--- Formula/upgradenvim.rb
+++ Formula/upgradenvim.rb
@@ -1,7 +1,7 @@
 class Upgradenvim < Formula
   homepage 'https://github.com/thecontinium/homebrew-upgradenvim/'
   url 'https://github.com/thecontinium/homebrew-upgradenvim.git'
-  version '1.0.3'
+  version '1.0.4'
 
   skip_clean 'bin'
 
