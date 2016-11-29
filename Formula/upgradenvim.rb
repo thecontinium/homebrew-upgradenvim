199d55d17b0470f907579fd8497e049463d982c7
thecontinium <thecontinium@outlook.com>
2016-11-29 15:40:26 +0800

    bumped

--- Formula/upgradenvim.rb
+++ Formula/upgradenvim.rb
@@ -1,7 +1,7 @@
 class Upgradenvim < Formula
   homepage 'https://github.com/thecontinium/homebrew-upgradenvim/'
   url 'https://github.com/thecontinium/homebrew-upgradenvim.git'
-  version '1.0.4'
+  version '1.0.5'
 
   skip_clean 'bin'
 
